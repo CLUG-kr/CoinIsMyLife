116992af5f7248220654ab44832d597c619f68f0
Jo Mingyu <owlsogul@gmail.com>
2018-01-28 04:46:08 +0900

    PriceManager 브릿지부분 완성

--- CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinSimulator.java
+++ CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinSimulator.java
@@ -1,9 +1,22 @@
 package kr.clug.coinismylife.simulator;
 
-public class CoinSimulator {
+import kr.clug.coinismylife.simulator.price.FetchEventHandler;
+import kr.clug.coinismylife.simulator.price.FetchEventListener;
+import kr.clug.coinismylife.simulator.price.PriceManager;
+
+public class CoinSimulator{
 	
+	private PriceManager priceManager;
+	public CoinSimulator() {
+		priceManager = new PriceManager();
+	}
 	
+	public PriceManager getPriceManager() {
+		return this.priceManager;
+	}
 	
-	
+	public void addFetchEventListener(FetchEventListener fetchEventListener) {
+		FetchEventHandler.addListener(fetchEventListener);
+	}
 	
 }
