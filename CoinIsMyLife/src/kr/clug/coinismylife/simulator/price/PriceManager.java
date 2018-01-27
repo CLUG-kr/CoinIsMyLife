7689cdaf7b7a94a2eb53a961bfa84e17358f9cf0
Jo Mingyu <owlsogul@gmail.com>
2018-01-28 04:40:09 +0900

    주석 추가

--- CoinIsMyLife/src/kr/clug/coinismylife/simulator/price/PriceManager.java
+++ CoinIsMyLife/src/kr/clug/coinismylife/simulator/price/PriceManager.java
@@ -9,15 +9,17 @@ public class PriceManager {
 	private PriceFetcher priceFetcher;
 	private volatile/*값 할당 등에서 atomic 하게 되도록 함.*/ double[] coinPrices = new double[CoinConstants.AMOUNT_COIN];
 	
+	// 해당 코인의 가격을 설정합니다.
 	private void setCoinPrice(int coinNum, double coinPrice) { 
 		this.coinPrices[coinNum] = coinPrice;
 	}
 	
+	// 해당 코인의 가격을 가져옵니다.
 	public double getCoinPrice(int coinNum) {
 		return this.coinPrices[coinNum];
 	}
 	
-	// price fetcher 객체를 생성하는 메소드입니다.
+	// price fetcher 객체를 생성하고 실행시키는 함수입니다.
 	// 생성 실패시 false를 반환합니다. 성공시 true를 반환합니다.
 	public boolean registerFetcher() {
 		try {
@@ -29,6 +31,8 @@ public class PriceManager {
 		}
 	}	
 	
+	// 해당 키를 가진 코인의 가격을 책정합니다.
+	// 만약 실패할 경우 false를 리턴합니다.
 	public boolean setPrice(String key, String value) {
 		try {
 			double val = Double.parseDouble(value);
@@ -43,5 +47,12 @@ public class PriceManager {
 			return false;
 		}
 	}
+	
+	// 모든 코인의 가격을 가져옵니다.
+	// 복사된 배열이 넘어갑니다.
+	public double[] getCoinPrices() {
+		return coinPrices.clone();
+	}
+	
 
 }
