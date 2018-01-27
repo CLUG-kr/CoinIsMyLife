019ab2ed09ef1d90c7c51c2d95c4a3dfa5f3a371
Jo Mingyu <owlsogul@gmail.com>
2018-01-28 07:49:16 +0900

    wallet 기본 구현

--- CoinIsMyLife/src/kr/clug/coinismylife/simulator/wallet/Wallet.java
+++ CoinIsMyLife/src/kr/clug/coinismylife/simulator/wallet/Wallet.java
@@ -1,32 +1,44 @@
 package kr.clug.coinismylife.simulator.wallet;
 
+import java.util.ArrayList;
+
+import kr.clug.coinismylife.simulator.CoinConstants;
+
 public final class Wallet {
 	
 	private String id;
 	private int money;
 	private double coinAmount[];
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
+	private ArrayList<Order> orderList;
+	public Wallet(String id) {
 		this.id = id;
-	}
-	public int getMoney() {
-		return money;
-	}
-	public void addMoney(int money) {
-		this.money += money;
-	}
-	public void setMoney(int money) {
-		this.money = money;
-	}
-	public double[] getCoinAmount() {
-		return coinAmount;
-	}
-	public void setCoin(double coinAmount[]) {
-		this.coinAmount = coinAmount;
+		this.orderList = new ArrayList<>();
 	}
 	
+	public String getId() {return id;}
+	
+	public int getMoney() {return money;}
+	public void addMoney(int money) {this.money += money;}
+	public void setMoney(int money) {this.money = money;}
+
+	
+	public double getCoinAmount(int index) {
+		if (0 <= index && index <= CoinConstants.AMOUNT_COIN -1) 
+			return coinAmount[index];
+		else 
+			return -1;
+	}
+	public void setCoinAmount(int index, double amount) {
+		if (0 <= index && index <= CoinConstants.AMOUNT_COIN -1) 
+			coinAmount[index] = amount;
+	}
+	public void addCoinAmount(int index, double amount) {
+		if (0 <= index && index <= CoinConstants.AMOUNT_COIN -1) 
+			coinAmount[index] += amount;
+	}
+	
+	public ArrayList<Order> getOrderList() {return orderList;}
+	public void addOrder(Order order) {orderList.add(order);}
 	
 	
 }
