c52f6fbed7013fab152591118ca31dff52cd09cb
Jo Mingyu <owlsogul@gmail.com>
2018-01-28 20:53:40 +0900

    키 값 가져오기

--- CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinConstants.java
+++ CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinConstants.java
@@ -2,19 +2,32 @@ package kr.clug.coinismylife.simulator;
 
 public class CoinConstants {
 
-	public static int AMOUNT_COIN = 12;
-	public static int INDEX_BTC   = 0;
-	public static int INDEX_ETH   = 1;
-	public static int INDEX_DASH  = 2;
-	public static int INDEX_LTC   = 3;
-	public static int INDEX_ETC   = 4;
-	public static int INDEX_XRP   = 5;
-	public static int INDEX_BCH   = 6;
-	public static int INDEX_XMR   = 7;
-	public static int INDEX_ZEC   = 8;
-	public static int INDEX_QTUM  = 9;
-	public static int INDEX_BTG   = 10;
-	public static int INDEX_EOS   = 11;
+	public static final int AMOUNT_COIN = 12;
+	public static final int INDEX_BTC   = 0;
+	public static final int INDEX_ETH   = 1;
+	public static final int INDEX_DASH  = 2;
+	public static final int INDEX_LTC   = 3;
+	public static final int INDEX_ETC   = 4;
+	public static final int INDEX_XRP   = 5;
+	public static final int INDEX_BCH   = 6;
+	public static final int INDEX_XMR   = 7;
+	public static final int INDEX_ZEC   = 8;
+	public static final int INDEX_QTUM  = 9;
+	public static final int INDEX_BTG   = 10;
+	public static final int INDEX_EOS   = 11;
+
+	public static final String KEY_BTC = "BTC";
+	public static final String KEY_ETH = "ETH";
+	public static final String KEY_DASH = "DASH";
+	public static final String KEY_LTC = "LTC";
+	public static final String KEY_ETC = "ETC";
+	public static final String KEY_XRP = "XRP";
+	public static final String KEY_BCH = "BCH";
+	public static final String KEY_XMR = "XMR";
+	public static final String KEY_ZEC = "ZEC";
+	public static final String KEY_QTUM = "QTUM";
+	public static final String KEY_BTG = "BTG";
+	public static final String KEY_EOS = "EOS";
 	
 	public static int getCoinIndex(String key) {
 		if (key.equals("BTC"))       return INDEX_BTC;
@@ -30,5 +43,23 @@ public class CoinConstants {
 		else if (key.equals("BTG"))  return INDEX_BTG;
 		else if (key.equals("EOS"))  return INDEX_EOS;
 		else return -1;
+	}
+	
+	public static String getCoinName(int id) {
+		switch(id) {
+		case INDEX_BTC: return KEY_BTC;
+		case INDEX_ETH: return KEY_ETH;
+		case INDEX_DASH: return KEY_DASH;
+		case INDEX_LTC: return KEY_LTC;
+		case INDEX_ETC: return KEY_ETC;
+		case INDEX_XRP: return KEY_XRP;
+		case INDEX_BCH: return KEY_BCH;
+		case INDEX_XMR: return KEY_XMR;
+		case INDEX_ZEC: return KEY_ZEC;
+		case INDEX_QTUM: return KEY_QTUM;
+		case INDEX_BTG: return KEY_BTG;
+		case INDEX_EOS: return KEY_EOS;
+		}
+		return null;
 	}
 }
