bbd2c15614e9601b6c2f9453773514c88f1af389
Jo Mingyu <owlsogul@gmail.com>
2018-01-30 03:43:35 +0900

    wallet manger 추가

--- CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinSimulator.java
+++ CoinIsMyLife/src/kr/clug/coinismylife/simulator/CoinSimulator.java
@@ -3,20 +3,32 @@ package kr.clug.coinismylife.simulator;
 import kr.clug.coinismylife.simulator.price.FetchEventHandler;
 import kr.clug.coinismylife.simulator.price.FetchEventListener;
 import kr.clug.coinismylife.simulator.price.PriceManager;
+import kr.clug.coinismylife.simulator.wallet.OrderCloseEventHandler;
+import kr.clug.coinismylife.simulator.wallet.OrderCloseEventListener;
+import kr.clug.coinismylife.simulator.wallet.WalletManager;
 
 public class CoinSimulator{
 	
 	private PriceManager priceManager;
+	private WalletManager walletManager;
 	public CoinSimulator() {
 		priceManager = new PriceManager();
+		walletManager = new WalletManager();
 	}
 	
 	public PriceManager getPriceManager() {
 		return this.priceManager;
 	}
 	
+	public WalletManager getWalletManager() {
+		return this.walletManager;
+	}
+	
 	public void addFetchEventListener(FetchEventListener fetchEventListener) {
 		FetchEventHandler.addListener(fetchEventListener);
 	}
 	
+	public void addOrderCloseEventListener(OrderCloseEventListener orderCloseEventListener) {
+		OrderCloseEventHandler.addListener(orderCloseEventListener);
+	}
 }
